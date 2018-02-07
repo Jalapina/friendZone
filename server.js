7324a85ed4d53f3588e3c207acc329448ae33237
Jalapina <davidpina14@gmail.com>
2018-02-06 20:16:42 -0600

    First commit

---
+++ server.js
@@ -0,0 +1,21 @@
+let express = require("express");
+let bodyParser = require("body-parser");
+let path = require("path");
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/client/dist'));
+
+require("./server/config/mongoose");
+require("./server/config/routes");
+
+app.all('*',(req,res,next) =>{
+    res.sendfile(path.resolve('./front-end/dist/index.html'))
+});
+
+app.listen(8000, function(){
+    console.log('Running on port 8000')
+});
+
