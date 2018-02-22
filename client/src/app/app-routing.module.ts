ab682cc6d176d8b35d60e1555db8ea40a0b641e5
Jalapina <davidpina14@gmail.com>
2018-02-21 20:28:53 -0600

    Login/Reg styling and added router file

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { Route } from '@angular/router/src/config';
+
+const routes: Routes = [
+  
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
