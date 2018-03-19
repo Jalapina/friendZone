5b8afcc9423be57ba922a02e27823e31a8c82b4b
Jalapina <davidpina14@gmail.com>
2018-03-19 09:46:29 -0500

    Started login/reg components

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,15 +2,34 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { Route } from '@angular/router/src/config';
-
+import { LandingComponent } from './landing/landing.component'
+import { HomeComponent } from './home/home.component'
+import { LoginComponent } from './login/login.component'
+import { RegisterComponent } from './register/register.component'
+ 
 const routes: Routes = [
-  
+  {
+    path: '',
+    pathMatch: 'full',
+    component: LandingComponent,
+  },
+  {
+    path: 'login',
+    pathMatch: 'full',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    pathMatch: 'full',
+    component: RegisterComponent,
+  },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes),
-    CommonModule
+    CommonModule,
   ],
   declarations: []
 })
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { LandingComponent } from './landing/landing.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
